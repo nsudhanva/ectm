11ecafae49798c86de3e1a4bb4cd5a9f9222a34f
shreyas <hello@shreyas.im>
2018-06-27 12:52:55 +0530

    ann works

--- ann.py
+++ ann.py
@@ -5,15 +5,29 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('engine_data.csv')
-X = dataset.iloc[:, 7:11].values
+X = dataset.iloc[:, 0:7].values
 y = dataset.iloc[:, [11]].values
 
 # Encoding categorical data
 y = y * 1
-X[5] = X[5] * 1
-X[6] = X[6] * 1
-X[7] = X[7] * 1
-X[8] = X[8] * 1
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_0 = LabelEncoder()
+X[:, 0] = labelencoder_X_0.fit_transform(X[:, 0])
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+onehotencoder = OneHotEncoder(categorical_features = [0, 1])
+X = onehotencoder.fit_transform(X).toarray()
+
+categories = [0, 1]
+dummies = []
+dummies_sum = 0
+
+for category in categories:
+    dummies_sum += (dataset.iloc[:, category].unique().size) * category
+    dummies.append(dummies_sum)
+
+X = np.delete(X, dummies, 1)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -35,15 +49,10 @@ from keras.models import load_model
 classifier = Sequential()
 
 # Adding the input layer and the first hidden layer
-# classifier.add(Dense(output_dim = 2, init = "uniform", activation='relu', input_dim = 2))
-classifier.add(Dense(activation="relu", input_dim=9, units=5, kernel_initializer="uniform"))
+classifier.add(Dense(activation="relu", input_dim=47, units=24, kernel_initializer="uniform"))
 
 # Adding the second hidden layer
-classifier.add(Dense(activation="relu", units=5, kernel_initializer="uniform"))
-
-#classifier.add(Dense(activation="relu", units=5, kernel_initializer="uniform"))
-#classifier.add(Dense(activation="relu", units=5, kernel_initializer="uniform"))
-#classifier.add(Dense(activation="relu", units=5, kernel_initializer="uniform"))
+classifier.add(Dense(activation="relu", units=24, kernel_initializer="uniform"))
 
 # Adding the output layer
 classifier.add(Dense(activation="sigmoid", units=1, kernel_initializer="uniform"))
@@ -52,16 +61,20 @@ classifier.add(Dense(activation="sigmoid", units=1, kernel_initializer="uniform"
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 # Fitting the ANN to the Training set
-classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 3)
+classifier.fit(X_train, y_train, batch_size = 100, nb_epoch = 100)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5)
 
-classifier.save('classifier.h5') # creates a HDF5 file 'my_model.h5'
-
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-accuracy = (cm[0][0] + cm[1][1])/ 20000
+accuracy = classifier.evaluate(X_test, y_test)[1]
+
+from sklearn.metrics import classification_report
+
+report = classification_report(y_test, y_pred)
+
+# classifier.save('classifier.h5') # creates a HDF5 file 'my_model.h5'
--- generate_data.py
+++ generate_data.py
@@ -17,83 +17,83 @@ max_n1 = 14000
 min_n1 = 8000
 max_n2 = 12000
 
+head = ["Engine", "Time", "Air Temperature", "Exhaust Gas Temperature", "Fuel Flow", "Low Pressure Fan Speed (N1)", "High Pressure Rotor Speed (N2)", "EGT Normal", "Fuel Flow Normal", "N1 Normal", "N2 Normal", "Normal"]                       
+engine_no = 0
+time = 0
 
-data = {}
-
-# i is for engines
-for i in range(0, 10000) :    
-    data[i] = {}  
+with open("engine_data.csv", "w") as csv_file :
+    writer = csv.writer(csv_file, delimiter=',')
+    writer.writerows([head])
     
-    # j is for time
-    for j in range(0, 10) :
+    for i in range(0, 100000):
         
-        data[i][j] = {}
-    
-        data[i][j]["atemp"] = int(np.random.uniform(min_atemp, max_atemp))
+        body = []
+        
+        if i % 5000 == 0:
+            engine_no += 1
+        
+        if time > 23:
+            time = 0
 
-        if data[i][j]["atemp"] >= -50 and data[i][j]["atemp"] <= -30 :
+        body.append(str(engine_no))
+        body.append(str(time))
+        
+        atemp = int(np.random.uniform(min_atemp, max_atemp))
+        body.append(str(atemp))
+
+        if atemp >= -50 and atemp <= -30 :
             
             egt = int(np.random.uniform(500, 1200))
             ff = int(np.random.uniform(4300, 5200))
             n1 = int(np.random.uniform(9000, 13000))
             n2 = int(np.random.uniform(9000, 13000))
             
-            data[i][j]["egt"] = egt
-            data[i][j]["ff"] = ff
-            data[i][j]["n1"] = n1
-            data[i][j]["n2"] = n2
-            
             if egt < 700 or egt > 1000 :
-                data[i][j]["egt_normal"] = False
+                egt_normal = False
             else:
-                data[i][j]["egt_normal"] = True
+                egt_normal = True
             
             if ff < 4500 or ff > 5000 :
-                data[i][j]["ff_normal"] = False
+                ff_normal = False
             else:
-                data[i][j]["ff_normal"] = True
+                ff_normal = True
 
             if n1 < 10000 or n1 > 12000 :
-                data[i][j]["n1_normal"] = False
+                n1_normal = False
             else:
-                data[i][j]["n1_normal"] = True            
+                n1_normal = True            
 
             if n2 < 10000 or n2 > 12000 :
-                data[i][j]["n2_normal"] = False
+                n2_normal = False
             else:
-                data[i][j]["n2_normal"] = True
+                n2_normal = True
             
-        elif data[i][j]["atemp"] > -30 and data[i][j]["atemp"] <= 0 :
+        elif atemp > -30 and atemp <= 0 :
             
             egt = int(np.random.uniform(800, 1700))
             ff = int(np.random.uniform(4600, 5400))
             n1 = int(np.random.uniform(10000, 14000))
             n2 = int(np.random.uniform(8000, 12000))
             
-            data[i][j]["egt"] = egt
-            data[i][j]["ff"] = ff
-            data[i][j]["n1"] = n1
-            data[i][j]["n2"] = n2
-            
             if egt < 1000 or egt > 1500 :
-                data[i][j]["egt_normal"] = False
+                egt_normal = False
             else:
-                data[i][j]["egt_normal"] = True
+                egt_normal = True
             
             if ff < 4800 or ff > 5200 :
-                data[i][j]["ff_normal"] = False
+                ff_normal = False
             else:
-                data[i][j]["ff_normal"] = True
+                ff_normal = True
 
             if n1 < 11000 or n1 > 13000 :
-                data[i][j]["n1_normal"] = False
+                n1_normal = False
             else:
-                data[i][j]["n1_normal"] = True            
+                n1_normal = True            
 
             if n2 < 9000 or n2 > 11000 :
-                data[i][j]["n2_normal"] = False
+                n2_normal = False
             else:
-                data[i][j]["n2_normal"] = True               
+                n2_normal = True               
             
         else :
             
@@ -102,68 +102,41 @@ for i in range(0, 10000) :
             n1 = int(np.random.uniform(11000, 15000))
             n2 = int(np.random.uniform(7000, 11000))
             
-            data[i][j]["egt"] = egt
-            data[i][j]["ff"] = ff
-            data[i][j]["n1"] = n1
-            data[i][j]["n2"] = n2
-            
             if egt < 1400 or egt > 1900 :
-                data[i][j]["egt_normal"] = False
+                egt_normal = False
             else:
-                data[i][j]["egt_normal"] = True
+                egt_normal = True
             
             if ff < 5100 or ff > 5500 :
-                data[i][j]["ff_normal"] = False
+                ff_normal = False
             else:
-                data[i][j]["ff_normal"] = True
+                ff_normal = True
 
             if n1 < 12000 or n1 > 14000 :
-                data[i][j]["n1_normal"] = False
+                n1_normal = False
             else:
-                data[i][j]["n1_normal"] = True            
+                n1_normal = True            
 
             if n2 < 8000 or n2 > 10000 :
-                data[i][j]["n2_normal"] = False
+                n2_normal = False
             else:
-                data[i][j]["n2_normal"] = True             
+                n2_normal = True             
         
-        if data[i][j]["egt_normal"] == False or data[i][j]["ff_normal"] == False or data[i][j]["n1_normal"] == False or data[i][j]["n2_normal"] == False :
-            data[i][j]["normal"] = False
+        if egt_normal == False or ff_normal == False or n1_normal == False or n2_normal == False :
+            normal = False
         else :
-            data[i][j]["normal"] = True
+            normal = True
 
-head_rows = ["Engine", "Time", "Air Temperature", "Exhaust Gas Temperature", "Fuel Flow", "Low Pressure Fan Speed (N1)", "High Pressure Rotor Speed (N2)", "EGT Normal", "Fuel Flow Normal", "N1 Normal", "N2 Normal", "Normal"]                       
-
-with open("engine_data_1.csv", "w") as csv_file :
-    writer = csv.writer(csv_file, delimiter=',')
-    writer.writerows([head_rows])
-    
-    for engine, value in data.items() :
+        body.append(str(egt))
+        body.append(str(ff))
+        body.append(str(n1))
+        body.append(str(n2))
+        body.append(str(egt_normal))
+        body.append(str(ff_normal))
+        body.append(str(n1_normal))
+        body.append(str(n2_normal))
+        body.append(str(normal))
         
-        for value, engine_data in value.items() :
-            
-            final_data = []
-            csv_data = []
-
-            csv_data.append(str(engine))
-            csv_data.append(str(value))
-            csv_data.append(str(engine_data['atemp']))
-            csv_data.append(str(engine_data['egt']))
-            csv_data.append(str(engine_data['ff']))
-            csv_data.append(str(engine_data['n1']))
-            csv_data.append(str(engine_data['n2']))
-            csv_data.append(str(engine_data['egt_normal']))
-            csv_data.append(str(engine_data['ff_normal']))
-            csv_data.append(str(engine_data['n1_normal']))
-            csv_data.append(str(engine_data['n2_normal']))
-            csv_data.append(str(engine_data['normal']))
-            
-            final_data.append(csv_data)
-            
-            writer.writerows(final_data)
-
-
-
-
-
-
+        writer.writerows([body])
+        
+        time += 1
