ef604cb13d9e5990f43f5aacc36efa2fa43bac9b
shreyas <hello@shreyas.im>
2018-07-06 16:36:12 +0530

    prediction works. need to change data generation

--- code/python/generate_data_error.py
+++ code/python/generate_data_error.py
@@ -9,11 +9,11 @@ prev_ff = 0
 prev_n1 = 0
 prev_n2 = 0
 
-with open("../../data/engine_data_1.csv", "w") as csv_file :
+with open("../../data/engine_data_error.csv", "w") as csv_file :
     writer = csv.writer(csv_file, delimiter=',')
     writer.writerows([head])
     
-    for engine_no in range(1, 2):
+    for engine_no in range(1, 101):
     
         body[engine_no] = {}
         
@@ -22,70 +22,68 @@ with open("../../data/engine_data_1.csv", "w") as csv_file :
             body_w = []
             
             body[engine_no][month] = {}
+            
             body[engine_no][month]['failure_prob'] = 0
             
             body[engine_no][month]['air_temp'] = np.random.randint(-15, 1)
             
-            if month < 18 :
+            if month <= 36:
                 body[engine_no][month]['epr'] = np.random.randint(48, 51)
-            else :
-                body[engine_no][month]['epr'] = np.random.randint(48, 55)
+            else:
+                body[engine_no][month]['epr'] = np.random.randint(49, 53)
                 
             if month == 1:
                 
-                body[engine_no][month]['egt'] = np.random.randint(1100, 80 + 1300 + abs((body[engine_no][month]['air_temp']*2)))
-                body[engine_no][month]['ff'] = np.random.randint(4800, 80 + 5000)
-                body[engine_no][month]['n1'] = np.random.randint(11000, 100 + 12500)
-                body[engine_no][month]['n2'] = int(np.random.uniform(9000, 100 + 11000))
-                body[engine_no][month]['noise'] = np.random.randint(120, 126)
+                body[engine_no][month]['egt'] = 1300
+                body[engine_no][month]['ff'] = 5000
+                body[engine_no][month]['n1'] = 12000
+                body[engine_no][month]['n2'] = 1000
+                body[engine_no][month]['noise'] = 130
                 
             else:
                 
-                prev_egt = body[engine_no][month - 1]['egt']
-                prev_ff = body[engine_no][month - 1]['ff']
-                prev_n1 = body[engine_no][month - 1]['n1']
-                prev_n2 = body[engine_no][month - 1]['n2']
-                prev_noise = body[engine_no][month - 1]['noise']
+                prev_failure_prob = body[engine_no][month-1]['failure_prob']
+                body[engine_no][month]['failure_prob'] = prev_failure_prob
                 
-                body[engine_no][month]['egt'] = np.random.randint(prev_egt, 80 + prev_egt + np.random.randint(1, 100) + abs((body[engine_no][month]['air_temp']*2)))
-                body[engine_no][month]['ff'] = np.random.randint(prev_ff, 80 + prev_ff + np.random.randint(1, 100))
-                body[engine_no][month]['n1'] = np.random.randint(prev_n1, 100 + prev_n1 + np.random.randint(1, 300))
-                body[engine_no][month]['n2'] = np.random.randint(prev_n2, 100 + prev_n2 + np.random.randint(1, 300))
-                body[engine_no][month]['noise'] = np.random.randint(prev_noise, prev_noise + np.random.randint(1, 3))
-                
-            if body[engine_no][month]['egt'] > 1900 :
+                body[engine_no][month]['egt'] = 1500 + np.random.randint((month-1)*11, month*11)
+                body[engine_no][month]['ff'] = 5200 + np.random.randint((month-1)*9, month*9)
+                body[engine_no][month]['n1'] = 13000 + np.random.randint((month-1)*35, month*35)
+                body[engine_no][month]['n2'] = 11000 + np.random.randint((month-1)*35, month*35)
+                body[engine_no][month]['noise'] = int(130 + month*0.2)
+            
+            if body[engine_no][month]['egt'] > 1750 :
                 body[engine_no][month]['egt_n'] = False
-                body[engine_no][month]['failure_prob'] += 18
+                body[engine_no][month]['failure_prob'] += 1
             else:
                 body[engine_no][month]['egt_n'] = True
             
-            if body[engine_no][month]['ff'] > 5500 :
+            if body[engine_no][month]['ff'] > 5350 :
                 body[engine_no][month]['ff_n'] = False
-                body[engine_no][month]['failure_prob'] += 18
+                body[engine_no][month]['failure_prob'] += 1
             else:
                 body[engine_no][month]['ff_n'] = True
     
-            if body[engine_no][month]['n1'] > 14000 :
+            if body[engine_no][month]['n1'] > 13500 :
                 body[engine_no][month]['n1_n'] = False
-                body[engine_no][month]['failure_prob'] += 18
+                body[engine_no][month]['failure_prob'] += 1
             else:
                 body[engine_no][month]['n1_n'] = True            
     
-            if body[engine_no][month]['n2'] > 12000 :
+            if body[engine_no][month]['n2'] > 11500 :
                 body[engine_no][month]['n2_n'] = False
-                body[engine_no][month]['failure_prob'] += 18
+                body[engine_no][month]['failure_prob'] += 1
             else:
                 body[engine_no][month]['n2_n'] = True
                 
-            if body[engine_no][month]['noise'] > 140 :
+            if body[engine_no][month]['noise'] > 137 :
                 body[engine_no][month]['noise_n'] = False
-                body[engine_no][month]['failure_prob'] += 18
+                body[engine_no][month]['failure_prob'] += 1
             else:
                 body[engine_no][month]['noise_n'] = True
                 
             if body[engine_no][month]['epr'] > 50 :
                 body[engine_no][month]['epr_n'] = False
-                body[engine_no][month]['failure_prob'] += 10
+                body[engine_no][month]['failure_prob'] += 1
             else:
                 body[engine_no][month]['epr_n'] = True
                 
@@ -93,6 +91,9 @@ with open("../../data/engine_data_1.csv", "w") as csv_file :
                 body[engine_no][month]['normal'] = False
             else :
                 body[engine_no][month]['normal'] = True
+                
+            if body[engine_no][month]['failure_prob'] > 100:
+                body[engine_no][month]['failure_prob'] = 100
     
             body_w.append(str(engine_no))
             body_w.append(str(month))
