1888e9c02ea543bc126d2fce4f900a348336c948
shreyas <hello@shreyas.im>
2018-07-11 18:19:50 +0530

    dataset is proper. need to apply ml

--- code/python/slr_engine_error_egt_1.py
+++ code/python/slr_engine_error_egt_1.py
@@ -4,14 +4,32 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 import pickle
 import csv
 
 # Importing the dataset
 dataset = pd.read_csv('../../data/engine_data_error_final_1.csv')
 
-X_data = dataset.loc[:, ['egt']]
+X_data = dataset.loc[:, ['month', 'egt']]
 X = X_data.values
+
+# Encoding categorical data
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Avoiding dummy variable trap
+categories = [0]
+dummies = []
+dummies_sum = 0
+
+for category in categories:
+    dummies_sum += (dataset.iloc[:, category].unique().size) * category
+    dummies.append(dummies_sum)
+
+X = np.delete(X, dummies, 1)
 
 # Loading and fitting the regression model
 with open('slr_egt.pkl', 'rb') as f:
